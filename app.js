739f89196c3695f0318acddda9e5cc2d003f08b0
genismoreno <genis.moreno.rene@gmail.com>
2020-03-15 23:49:06 +0100

    adding DB

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
+var phrasesRouter = require('./routes/phrases');
 
 var app = express();
+
+//setup db
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/golden', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB successfully');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +31,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
+app.use('/phrases', phrasesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
